06341837df8114f636a9ed3ef04ea0b960e107b4
Huang Tianyu <huangtianyu_hty@163.com>
2017-11-23 16:48:55 +0800

    将原来的调用方式改为继承方式

--- 代理模式/main.js
+++ 代理模式/main.js
@@ -1,3 +1,13 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = Object.setPrototypeOf ||
+        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 /* 生成一张图片 */
 var myImg = /** @class */ (function () {
     function myImg(where) {
@@ -14,46 +24,47 @@ var myImg = /** @class */ (function () {
     return myImg;
 }());
 /*
-等待网络图片加载完成之前先设置一张loading菊花图，
+封装一个类嗲用前一个类，等待网络图片加载完成之前先设置一张loading菊花图，
 加载完成后将图片换成该显示的图片
 */
-var ProxImg = /** @class */ (function () {
+var ProxImg = /** @class */ (function (_super) {
+    __extends(ProxImg, _super);
     function ProxImg(where) {
-        this.img = new Image;
-        this.where = where;
+        var _this = _super.call(this, where) || this;
+        _this.img = new Image;
+        return _this;
     }
     ProxImg.prototype.setSrc = function (src) {
-        var newImg = new myImg(this.where);
-        newImg.setSrc('loading.gif');
+        var _this = this;
+        _super.prototype.setSrc.call(this, 'loading.gif');
         this.img.src = src;
         this.img.onload = function () {
-            newImg.setSrc(src);
+            _super.prototype.setSrc.call(_this, src);
         };
     };
     return ProxImg;
-}());
-/*
-    图片的显示列表
-*/
-var ImgList = /** @class */ (function () {
-    function ImgList(list, place) {
+}(myImg));
+/*循环调用前一个类，插入图片的显示列表*/
+var LoadImgList = /** @class */ (function () {
+    function LoadImgList(list, place) {
         list.map(function (item) {
             var img_1 = new ProxImg(place);
             img_1.setSrc(item);
         });
     }
-    return ImgList;
+    return LoadImgList;
 }());
+/*  Test  */
 var place = document.getElementsByTagName('div')[0];
 var list = [
     "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1512024296&di=dcd04a90b12cd3f135beb4cae88d85dc&imgtype=jpg&er=1&src=http%3A%2F%2Fwww.louisch.com%2Fwp-content%2Fuploads%2F2012%2F01%2FIMG_9005.jpg",
     "https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=1123931301,3549061685&fm=27&gp=0.jpg",
     "https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=3687059170,1699182582&fm=27&gp=0.jpg"
 ];
-new ImgList(list, place);
+new LoadImgList(list, place); //插入显示图片列表
 // let place = document.getElementsByTagName('div')[0];
 // let img_1 = new ProxImg(place);
 // img_1.setSrc("https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1512024296&di=dcd04a90b12cd3f135beb4cae88d85dc&imgtype=jpg&er=1&src=http%3A%2F%2Fwww.louisch.com%2Fwp-content%2Fuploads%2F2012%2F01%2FIMG_9005.jpg");
 /*
-    要遵守的原则是：单一职能原则
+    要遵守的原则是：单一职能原则，将复杂的逻辑封装带不同的类中，每一个类只负责一个功能
 */
--- 代理模式/main.ts
+++ 代理模式/main.ts
@@ -12,29 +12,25 @@ class myImg{
     }
 }
 /* 
-等待网络图片加载完成之前先设置一张loading菊花图，
+封装一个类嗲用前一个类，等待网络图片加载完成之前先设置一张loading菊花图，
 加载完成后将图片换成该显示的图片 
 */
-class ProxImg{
+class ProxImg extends myImg{
     img = new Image;
-    where: HTMLElement
     constructor( where: HTMLElement){
-        this.where = where;
+        super(where);
     }
     setSrc(src:string){
-        let newImg = new myImg(this.where);
-        newImg.setSrc('loading.gif');
+        super.setSrc('loading.gif')
         this.img.src = src;
-        this.img.onload = function(){
-            newImg.setSrc(src);
+        this.img.onload = ()=>{
+            super.setSrc(src);
         }
     }
 }
 
-/* 
-    图片的显示列表
-*/
-class ImgList{
+/*循环调用前一个类，插入图片的显示列表*/
+class LoadImgList{
     constructor(list:string[],place:HTMLElement){
         list.map((item)=>{
             let img_1 = new ProxImg(place);
@@ -43,14 +39,14 @@ class ImgList{
     }
 }
 
-
+/*  Test  */
 let place = document.getElementsByTagName('div')[0];
 let list = [
     "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1512024296&di=dcd04a90b12cd3f135beb4cae88d85dc&imgtype=jpg&er=1&src=http%3A%2F%2Fwww.louisch.com%2Fwp-content%2Fuploads%2F2012%2F01%2FIMG_9005.jpg",
     "https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=1123931301,3549061685&fm=27&gp=0.jpg",
     "https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=3687059170,1699182582&fm=27&gp=0.jpg"
 ];
-new ImgList(list,place);
+new LoadImgList(list,place); //插入显示图片列表
 
 
 // let place = document.getElementsByTagName('div')[0];
@@ -58,5 +54,5 @@ new ImgList(list,place);
 // img_1.setSrc("https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1512024296&di=dcd04a90b12cd3f135beb4cae88d85dc&imgtype=jpg&er=1&src=http%3A%2F%2Fwww.louisch.com%2Fwp-content%2Fuploads%2F2012%2F01%2FIMG_9005.jpg");
 
 /* 
-    要遵守的原则是：单一职能原则
+    要遵守的原则是：单一职能原则，将复杂的逻辑封装带不同的类中，每一个类只负责一个功能
 */
